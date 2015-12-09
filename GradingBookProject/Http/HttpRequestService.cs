ed75e1f637bf924a6e6d9ad9f150455135216d86
jedrekf <jedrek@fijalkowscy.com>
2015-12-09 22:11:50 +0100

    requests for get element by id, delete, update, add

--- GradingBookProject/Http/HttpRequestService.cs
+++ GradingBookProject/Http/HttpRequestService.cs
@@ -61,6 +61,103 @@ namespace GradingBookProject.Http
             }
         }
 
+        public async Task<T> GetOne(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //GET T/{id} to get specific
+                HttpResponseMessage response = await client.GetAsync(url + "/" + id.ToString());
+                if (response.IsSuccessStatusCode)
+                {
+                    T responseObject = await response.Content.ReadAsAsync<T>();
+                    return responseObject;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+
+        public async Task<T> PostOne(T o) {
+           
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                
+                //POST to create new
+                HttpResponseMessage response = await client.PostAsJsonAsync(url, o);
+                if (response.IsSuccessStatusCode)
+                {
+                    T responseObject = await response.Content.ReadAsAsync<T>();
+                    return responseObject;
+                }
+                else {
+                    return default(T);
+                }
+            }
+        }
+
+        public async Task<T> UpdateOne(int id, T o)
+        {
+            
+            
+            using (var client = new HttpClient())
+            {
+                string updateUrl = url + "/" + id.ToString();
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(updateUrl);
+              
+                if (response.IsSuccessStatusCode)
+                {
+                    T responseObject = await response.Content.ReadAsAsync<T>();
+                    //PUT to update
+                    response = await client.PutAsJsonAsync(updateUrl, o);
+                    return responseObject;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        public async Task<T> DeleteOne(int id)
+        {
+
+            using (var client = new HttpClient())
+            {
+                string deleteUrl = url + "/" + id.ToString();
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(deleteUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    T responseObject = await response.Content.ReadAsAsync<T>();
+                    //DELETE to remove
+                    response = await client.DeleteAsync(deleteUrl);
+                    return responseObject;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
 
     }
 }
