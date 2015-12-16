e24846a3901749bc08222a36e256babc1cf5e65f
Adam Malewski <adam.malewski.wp.pl>
2015-12-16 10:30:14 +0100

    Fixed: GradingBokkEntities

--- GradingBookApi/Models/AdoModel.Context.cs
+++ GradingBookApi/Models/AdoModel.Context.cs
@@ -13,10 +13,10 @@ namespace GradingBookApi.Models
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class Entities : DbContext
+    public partial class GradingBookEntities : DbContext
     {
-        public Entities()
-            : base("name=Entities")
+        public GradingBookEntities()
+            : base("name=GradingBookEntities")
         {
         }
     
---
+++ GradingBookProject/Forms/YearInGroupForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GradingBookProject.Forms
+{
+    partial class YearInGroupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.tableUsers = new System.Windows.Forms.TableLayoutPanel();
+            this.tableSubjects = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add year";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(93, 12);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit year";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // tableUsers
+            // 
+            this.tableUsers.ColumnCount = 2;
+            this.tableUsers.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableUsers.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableUsers.Location = new System.Drawing.Point(12, 41);
+            this.tableUsers.Name = "tableUsers";
+            this.tableUsers.RowCount = 2;
+            this.tableUsers.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableUsers.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableUsers.Size = new System.Drawing.Size(596, 180);
+            this.tableUsers.TabIndex = 2;
+            // 
+            // tableSubjects
+            // 
+            this.tableSubjects.ColumnCount = 2;
+            this.tableSubjects.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableSubjects.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableSubjects.Location = new System.Drawing.Point(12, 240);
+            this.tableSubjects.Name = "tableSubjects";
+            this.tableSubjects.RowCount = 2;
+            this.tableSubjects.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableSubjects.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableSubjects.Size = new System.Drawing.Size(596, 195);
+            this.tableSubjects.TabIndex = 3;
+            // 
+            // YearInGroupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 447);
+            this.Controls.Add(this.tableSubjects);
+            this.Controls.Add(this.tableUsers);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "YearInGroupForm";
+            this.Text = "YearInGroupForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.TableLayoutPanel tableUsers;
+        private System.Windows.Forms.TableLayoutPanel tableSubjects;
+    }
+}
