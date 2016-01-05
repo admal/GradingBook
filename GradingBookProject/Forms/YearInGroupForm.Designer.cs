03652c0c1af51b7a599b3bed75a6b6d3e1ff52a5
jedrekf <jedrek@fijalkowscy.com>
2016-01-05 16:37:04 +0100

    Reworked YearsInGroup form

--- GradingBookProject/Forms/YearInGroupForm.Designer.cs
+++ GradingBookProject/Forms/YearInGroupForm.Designer.cs
@@ -34,15 +34,21 @@
             this.dataPanel = new System.Windows.Forms.Panel();
             this.usersGridView = new System.Windows.Forms.DataGridView();
             this.subjectsGridView = new System.Windows.Forms.DataGridView();
+            this.usernameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.surnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.usersBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.teachermailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.subdescDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.subjectsViewModelBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.teachermailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.subjectsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.buttonPanel.SuspendLayout();
             this.dataPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.usersGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.subjectsGridView)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.subjectsViewModelBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.subjectsBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonPanel
@@ -76,10 +82,23 @@
             // 
             // usersGridView
             // 
+            this.usersGridView.AllowUserToAddRows = false;
+            this.usersGridView.AllowUserToDeleteRows = false;
+            this.usersGridView.AutoGenerateColumns = false;
+            this.usersGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.usersGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.usersGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.usernameDataGridViewTextBoxColumn,
+            this.emailDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn1,
+            this.surnameDataGridViewTextBoxColumn});
+            this.usersGridView.DataSource = this.usersBindingSource;
             this.usersGridView.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.usersGridView.Location = new System.Drawing.Point(0, 208);
+            this.usersGridView.MultiSelect = false;
             this.usersGridView.Name = "usersGridView";
+            this.usersGridView.ReadOnly = true;
+            this.usersGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.usersGridView.Size = new System.Drawing.Size(620, 205);
             this.usersGridView.TabIndex = 1;
             // 
@@ -88,44 +107,78 @@
             this.subjectsGridView.AllowUserToAddRows = false;
             this.subjectsGridView.AllowUserToDeleteRows = false;
             this.subjectsGridView.AutoGenerateColumns = false;
+            this.subjectsGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.subjectsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.subjectsGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.nameDataGridViewTextBoxColumn,
-            this.teachermailDataGridViewTextBoxColumn,
-            this.subdescDataGridViewTextBoxColumn});
-            this.subjectsGridView.DataSource = this.subjectsViewModelBindingSource;
+            this.subdescDataGridViewTextBoxColumn,
+            this.teachermailDataGridViewTextBoxColumn});
+            this.subjectsGridView.DataSource = this.subjectsBindingSource;
             this.subjectsGridView.Dock = System.Windows.Forms.DockStyle.Top;
             this.subjectsGridView.Location = new System.Drawing.Point(0, 0);
+            this.subjectsGridView.MultiSelect = false;
             this.subjectsGridView.Name = "subjectsGridView";
             this.subjectsGridView.ReadOnly = true;
+            this.subjectsGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.subjectsGridView.Size = new System.Drawing.Size(620, 202);
             this.subjectsGridView.TabIndex = 0;
+            // 
+            // usernameDataGridViewTextBoxColumn
+            // 
+            this.usernameDataGridViewTextBoxColumn.DataPropertyName = "username";
+            this.usernameDataGridViewTextBoxColumn.HeaderText = "username";
+            this.usernameDataGridViewTextBoxColumn.Name = "usernameDataGridViewTextBoxColumn";
+            this.usernameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "email";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "email";
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            this.emailDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameDataGridViewTextBoxColumn1
+            // 
+            this.nameDataGridViewTextBoxColumn1.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn1.HeaderText = "name";
+            this.nameDataGridViewTextBoxColumn1.Name = "nameDataGridViewTextBoxColumn1";
+            this.nameDataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // surnameDataGridViewTextBoxColumn
+            // 
+            this.surnameDataGridViewTextBoxColumn.DataPropertyName = "surname";
+            this.surnameDataGridViewTextBoxColumn.HeaderText = "surname";
+            this.surnameDataGridViewTextBoxColumn.Name = "surnameDataGridViewTextBoxColumn";
+            this.surnameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // usersBindingSource
+            // 
+            this.usersBindingSource.DataSource = typeof(GradingBookProject.ViewModels.UsersViewModel);
             // 
             // nameDataGridViewTextBoxColumn
             // 
             this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
-            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "name";
             this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
             this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // subdescDataGridViewTextBoxColumn
+            // 
+            this.subdescDataGridViewTextBoxColumn.DataPropertyName = "sub_desc";
+            this.subdescDataGridViewTextBoxColumn.HeaderText = "Subject description";
+            this.subdescDataGridViewTextBoxColumn.Name = "subdescDataGridViewTextBoxColumn";
+            this.subdescDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // teachermailDataGridViewTextBoxColumn
             // 
             this.teachermailDataGridViewTextBoxColumn.DataPropertyName = "teacher_mail";
-            this.teachermailDataGridViewTextBoxColumn.HeaderText = "Teacher\'s mail";
+            this.teachermailDataGridViewTextBoxColumn.HeaderText = "Teachers email address";
             this.teachermailDataGridViewTextBoxColumn.Name = "teachermailDataGridViewTextBoxColumn";
             this.teachermailDataGridViewTextBoxColumn.ReadOnly = true;
             // 
-            // subdescDataGridViewTextBoxColumn
+            // subjectsBindingSource
             // 
-            this.subdescDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.subdescDataGridViewTextBoxColumn.DataPropertyName = "sub_desc";
-            this.subdescDataGridViewTextBoxColumn.HeaderText = "Description";
-            this.subdescDataGridViewTextBoxColumn.Name = "subdescDataGridViewTextBoxColumn";
-            this.subdescDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // subjectsViewModelBindingSource
-            // 
-            this.subjectsViewModelBindingSource.DataSource = typeof(GradingBookProject.ViewModels.SubjectsViewModel);
+            this.subjectsBindingSource.DataSource = typeof(GradingBookProject.ViewModels.SubjectsViewModel);
             // 
             // YearInGroupForm
             // 
@@ -140,7 +193,8 @@
             this.dataPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.usersGridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.subjectsGridView)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.subjectsViewModelBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.subjectsBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -151,11 +205,16 @@
         private System.Windows.Forms.Panel dataPanel;
         private System.Windows.Forms.DataGridView usersGridView;
         private System.Windows.Forms.DataGridView subjectsGridView;
-        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn teachermailDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn subdescDataGridViewTextBoxColumn;
-        private System.Windows.Forms.BindingSource subjectsViewModelBindingSource;
+        private System.Windows.Forms.BindingSource subjectsBindingSource;
         private System.Windows.Forms.Button btnAddSubject;
+        private System.Windows.Forms.BindingSource usersBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn usernameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn surnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn subdescDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn teachermailDataGridViewTextBoxColumn;
 
     }
 }
