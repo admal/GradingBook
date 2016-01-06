7da3153d675581b446117ed4241132465d2b1fd2
Adam Malewski <adam.malewski.wp.pl>
2016-01-06 17:11:23 +0100

    editing deleting year in YearInGroupForm

--- GradingBookProject/Forms/YearInGroupForm.Designer.cs
+++ GradingBookProject/Forms/YearInGroupForm.Designer.cs
@@ -30,15 +30,17 @@
         {
             this.components = new System.ComponentModel.Container();
             this.buttonPanel = new System.Windows.Forms.Panel();
+            this.btnDeleteYear = new System.Windows.Forms.Button();
+            this.btnEditYear = new System.Windows.Forms.Button();
             this.btnAddSubject = new System.Windows.Forms.Button();
             this.dataPanel = new System.Windows.Forms.Panel();
             this.usersGridView = new System.Windows.Forms.DataGridView();
-            this.subjectsGridView = new System.Windows.Forms.DataGridView();
             this.usernameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.nameDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.surnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.usersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.subjectsGridView = new System.Windows.Forms.DataGridView();
             this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.subdescDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.teachermailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -46,13 +48,15 @@
             this.buttonPanel.SuspendLayout();
             this.dataPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.usersGridView)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.subjectsGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.subjectsGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.subjectsBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonPanel
             // 
+            this.buttonPanel.Controls.Add(this.btnDeleteYear);
+            this.buttonPanel.Controls.Add(this.btnEditYear);
             this.buttonPanel.Controls.Add(this.btnAddSubject);
             this.buttonPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.buttonPanel.Location = new System.Drawing.Point(0, 0);
@@ -60,6 +64,26 @@
             this.buttonPanel.Size = new System.Drawing.Size(620, 34);
             this.buttonPanel.TabIndex = 4;
             this.buttonPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnDeleteYear
+            // 
+            this.btnDeleteYear.Location = new System.Drawing.Point(174, 3);
+            this.btnDeleteYear.Name = "btnDeleteYear";
+            this.btnDeleteYear.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteYear.TabIndex = 4;
+            this.btnDeleteYear.Text = "Delete year";
+            this.btnDeleteYear.UseVisualStyleBackColor = true;
+            this.btnDeleteYear.Click += new System.EventHandler(this.DeleteYearClick);
+            // 
+            // btnEditYear
+            // 
+            this.btnEditYear.Location = new System.Drawing.Point(93, 3);
+            this.btnEditYear.Name = "btnEditYear";
+            this.btnEditYear.Size = new System.Drawing.Size(75, 23);
+            this.btnEditYear.TabIndex = 3;
+            this.btnEditYear.Text = "Edit year";
+            this.btnEditYear.UseVisualStyleBackColor = true;
+            this.btnEditYear.Click += new System.EventHandler(this.EditYearClick);
             // 
             // btnAddSubject
             // 
@@ -102,27 +126,6 @@
             this.usersGridView.Size = new System.Drawing.Size(620, 205);
             this.usersGridView.TabIndex = 1;
             // 
-            // subjectsGridView
-            // 
-            this.subjectsGridView.AllowUserToAddRows = false;
-            this.subjectsGridView.AllowUserToDeleteRows = false;
-            this.subjectsGridView.AutoGenerateColumns = false;
-            this.subjectsGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.subjectsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.subjectsGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.nameDataGridViewTextBoxColumn,
-            this.subdescDataGridViewTextBoxColumn,
-            this.teachermailDataGridViewTextBoxColumn});
-            this.subjectsGridView.DataSource = this.subjectsBindingSource;
-            this.subjectsGridView.Dock = System.Windows.Forms.DockStyle.Top;
-            this.subjectsGridView.Location = new System.Drawing.Point(0, 0);
-            this.subjectsGridView.MultiSelect = false;
-            this.subjectsGridView.Name = "subjectsGridView";
-            this.subjectsGridView.ReadOnly = true;
-            this.subjectsGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.subjectsGridView.Size = new System.Drawing.Size(620, 202);
-            this.subjectsGridView.TabIndex = 0;
-            // 
             // usernameDataGridViewTextBoxColumn
             // 
             this.usernameDataGridViewTextBoxColumn.DataPropertyName = "username";
@@ -154,6 +157,27 @@
             // usersBindingSource
             // 
             this.usersBindingSource.DataSource = typeof(GradingBookProject.ViewModels.UsersViewModel);
+            // 
+            // subjectsGridView
+            // 
+            this.subjectsGridView.AllowUserToAddRows = false;
+            this.subjectsGridView.AllowUserToDeleteRows = false;
+            this.subjectsGridView.AutoGenerateColumns = false;
+            this.subjectsGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.subjectsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.subjectsGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.subdescDataGridViewTextBoxColumn,
+            this.teachermailDataGridViewTextBoxColumn});
+            this.subjectsGridView.DataSource = this.subjectsBindingSource;
+            this.subjectsGridView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.subjectsGridView.Location = new System.Drawing.Point(0, 0);
+            this.subjectsGridView.MultiSelect = false;
+            this.subjectsGridView.Name = "subjectsGridView";
+            this.subjectsGridView.ReadOnly = true;
+            this.subjectsGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.subjectsGridView.Size = new System.Drawing.Size(620, 202);
+            this.subjectsGridView.TabIndex = 0;
             // 
             // nameDataGridViewTextBoxColumn
             // 
@@ -192,8 +216,8 @@
             this.buttonPanel.ResumeLayout(false);
             this.dataPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.usersGridView)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.subjectsGridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.subjectsGridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.subjectsBindingSource)).EndInit();
             this.ResumeLayout(false);
 
@@ -215,6 +239,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn subdescDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn teachermailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button btnDeleteYear;
+        private System.Windows.Forms.Button btnEditYear;
 
     }
 }
